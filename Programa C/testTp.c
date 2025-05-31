cc5f518508a54489a95d5cda43e5673974032f1e
Isa2322 <altamirandaisaias@gmail.com>
2025-05-31 01:32:07 -0300

    Update testTp.c

--- Programa C/testTp.c
+++ Programa C/testTp.c
@@ -12,6 +12,14 @@
 // Constantes: se producirán 10 autos, y el tiempo base por estación es 5 segundos.
 #define MAX_AUTOS 10
 #define TIEMPO_ESTACION 5
+#define ESTACIONES_TRABAJO 5
+
+// Indices de cada estacion
+#define ESTACION_CHASIS 0
+#define ESTACION_MOTOR 1
+#define ESTACION_VIDRIOS 2
+#define ESTACION_INTERIORES 3
+#define ESTACION_PINTURA 4
 
 // Constantes de Precio y Tiempo cualquier cosa se cambia desde aca
 // revisar el tema de la randomizacion en precios y tiempo
@@ -24,11 +32,11 @@
 // Tiempo, por ahora lo dejo en segundos, eso lo cambiamos despues desde aca tambien
 // Revisar si el tiempo es variable o no
 // de momento dejo todo en 5
-#define TIEMPO_CHASIS 5
-#define TIEMPO_MOTOR 5
-#define TIEMPO_VIDRIOS 5
-#define TIEMPO_INTERIORES 5
-#define TIEMPO_PINTURA 5
+#define TIEMPO_CHASIS 8
+#define TIEMPO_MOTOR 12
+#define TIEMPO_VIDRIOS 3
+#define TIEMPO_INTERIORES 10
+#define TIEMPO_PINTURA 6
 
 // Memoria compartda
 #define NombreMemoria "miMemoria"
@@ -43,6 +51,12 @@ typedef struct
     int partes_defectuosas;
     float tiempo_total_produccion;
     float costo_total;
+
+    int defectos_por_pieza[ESTACIONES_TRABAJO];       // índice 0: chasis, 1: motor, ...
+    float tiempo_por_pieza[ESTACIONES_TRABAJO];       // suma de tiempos por pieza
+    float costo_pieza[ESTACIONES_TRABAJO];            // costo acumulado por tipo de pieza
+    float costo_defectuosas[ESTACIONES_TRABAJO];      // suma de costos por piezas defectuosas
+
 } t_estadisticas;
 
 /////////////////////////////////////////
@@ -67,9 +81,12 @@ void chasis(t_estadisticas *stat, sem_t *sem)
     // Se repite dentro del while si se cambia, hay que hacerlo en ambos lados
     // Se borran las variables costo y tiempo y se suman las constantes directamente en la variable estadisticas
     // En todo caso hay que cambiarlo en todas las estaciones de trabajo
-    int costo = (rand() % (PRECIO_CHASIS / 2)) + PRECIO_CHASIS;
+    int costo = PRECIO_CHASIS;
     int tiempo = (rand() % (TIEMPO_CHASIS / 2)) + TIEMPO_CHASIS;
-
+    int cantidad_defectuosos;
+    
+    if(prob<90)
+        printf("Chasis instalado\n");
     while (prob > 90)
     {
         printf("chasis defectuoso, cambiando chasis...\n");
@@ -77,18 +94,23 @@ void chasis(t_estadisticas *stat, sem_t *sem)
         stat->partes_defectuosas++;
         stat->tiempo_total_produccion += tiempo;
         stat->costo_total += costo;
+        stat->defectos_por_pieza[ESTACION_CHASIS] += 1;
+        stat->tiempo_por_pieza[ESTACION_CHASIS] += tiempo;
+        stat->costo_pieza[ESTACION_CHASIS] += PRECIO_CHASIS;
+        stat->costo_defectuosas[ESTACION_CHASIS] += PRECIO_CHASIS;
         sem_post(sem); // V()
         sleep(TIEMPO_ESTACION); // Opcional
 
         prob = ((rand() % 100) + 1);
 
-        costo = (rand() % (PRECIO_CHASIS / 2)) + PRECIO_CHASIS;
         tiempo = (rand() % (TIEMPO_CHASIS / 2)) + TIEMPO_CHASIS;
     }
 
     sem_wait(sem); // P()
-    stat->costo_total += costo;
+    stat->costo_total += PRECIO_CHASIS;
     stat->tiempo_total_produccion += tiempo;
+    stat->tiempo_por_pieza[ESTACION_CHASIS] += tiempo;
+    stat->costo_pieza[ESTACION_CHASIS] += PRECIO_CHASIS;
     sem_post(sem); // V()
 }
 
@@ -96,28 +118,35 @@ void motor(t_estadisticas *stat, sem_t *sem)
 {
     int prob = (rand() % 100) + 1;
 
-    int costo = (rand() % (PRECIO_MOTOR / 2)) + PRECIO_MOTOR;
     int tiempo = (rand() % (TIEMPO_MOTOR / 2)) + TIEMPO_MOTOR;
 
+
+    if(prob<=90)
+        printf("motor instalado\n");
     while (prob > 90)
     {
         printf("motor defectuoso, cambiando motor...\n");
         sem_wait(sem); // P()
         stat->partes_defectuosas++;
         stat->tiempo_total_produccion += tiempo;
-        stat->costo_total += costo;
+        stat->costo_total += PRECIO_MOTOR;
+        stat->defectos_por_pieza[ESTACION_MOTOR] += 1;
+        stat->tiempo_por_pieza[ESTACION_MOTOR] += tiempo;
+        stat->costo_pieza[ESTACION_MOTOR] += PRECIO_MOTOR;
+        stat->costo_defectuosas[ESTACION_MOTOR] += PRECIO_MOTOR;
         sem_post(sem); // V()
-        sleep(TIEMPO_ESTACION);
+        sleep(TIEMPO_ESTACION); 
 
         prob = (rand() % 100) + 1;
 
-        costo = (rand() % (PRECIO_MOTOR / 2)) + PRECIO_MOTOR;
         tiempo = (rand() % (TIEMPO_MOTOR / 2)) + TIEMPO_MOTOR;
     }
 
     sem_wait(sem); // P()
-    stat->costo_total += costo;
+    stat->costo_total += PRECIO_MOTOR;
     stat->tiempo_total_produccion += tiempo;
+    stat->tiempo_por_pieza[ESTACION_MOTOR] += tiempo;
+    stat->costo_pieza[ESTACION_MOTOR] += PRECIO_MOTOR;
     sem_post(sem); // V()
 }
 
@@ -126,28 +155,34 @@ void vidrios(t_estadisticas *stat, sem_t *sem)
 {
     int prob = (rand() % 100) + 1;
 
-    int costo = (rand() % (PRECIO_VIDRIOS / 2)) + PRECIO_VIDRIOS;
     int tiempo = (rand() % (TIEMPO_VIDRIOS / 2)) + TIEMPO_VIDRIOS;
 
+    if(prob<=90)
+        printf("vidrios instalado\n");
     while (prob > 90)
     {
         printf("vidrios defectuosos, cambiando vidrios...\n");
         sem_wait(sem); // P()
         stat->partes_defectuosas++;
         stat->tiempo_total_produccion += tiempo;
-        stat->costo_total += costo;
+        stat->costo_total += PRECIO_VIDRIOS;
+        stat->defectos_por_pieza[ESTACION_VIDRIOS] += 1;
+        stat->tiempo_por_pieza[ESTACION_VIDRIOS] += tiempo;
+        stat->costo_pieza[ESTACION_VIDRIOS] += PRECIO_VIDRIOS;
+        stat->costo_defectuosas[ESTACION_VIDRIOS] += PRECIO_VIDRIOS;
         sem_post(sem); // V()
 
         sleep(TIEMPO_ESTACION);
 
         prob = (rand() % 100) + 1;
-        costo = (rand() % (PRECIO_VIDRIOS / 2)) + PRECIO_VIDRIOS;
         tiempo = (rand() % (TIEMPO_VIDRIOS / 2)) + TIEMPO_VIDRIOS;
     }
 
     sem_wait(sem); // P()
-    stat->costo_total += costo;
+    stat->costo_total += PRECIO_VIDRIOS;
     stat->tiempo_total_produccion += tiempo;
+    stat->tiempo_por_pieza[ESTACION_VIDRIOS] += tiempo;
+    stat->costo_pieza[ESTACION_VIDRIOS] += PRECIO_VIDRIOS;
     sem_post(sem); // V()
 }
 
@@ -156,29 +191,35 @@ void interior(t_estadisticas *stat, sem_t *sem)
 {
     int prob = (rand() % 100) + 1;
 
-    int costo = (rand() % (PRECIO_INTERIORES / 2)) + PRECIO_INTERIORES;
     int tiempo = (rand() % (TIEMPO_INTERIORES / 2)) + TIEMPO_INTERIORES;
 
+    if(prob<=90)
+        printf("Interior instalado\n");
     while (prob > 90)
     {
         printf("interiores defectuosos, cambiando interiores...\n");
         sem_wait(sem); // P()
         stat->partes_defectuosas++;
         stat->tiempo_total_produccion += tiempo;
-        stat->costo_total += costo;
+        stat->costo_total += PRECIO_INTERIORES;
+        stat->defectos_por_pieza[ESTACION_INTERIORES] += 1;
+        stat->tiempo_por_pieza[ESTACION_INTERIORES] += tiempo;
+        stat->costo_pieza[ESTACION_INTERIORES] += PRECIO_INTERIORES;
+        stat->costo_defectuosas[ESTACION_INTERIORES] += PRECIO_INTERIORES;
         sem_post(sem); // V()
 
         sleep(TIEMPO_ESTACION);
 
         prob = (rand() % 100) + 1;
 
-        costo = (rand() % (PRECIO_INTERIORES / 2)) + PRECIO_INTERIORES;
         tiempo = (rand() % (TIEMPO_INTERIORES / 2)) + TIEMPO_INTERIORES;
     }
 
     sem_wait(sem); // P()
-    stat->costo_total += costo;
+    stat->costo_total += PRECIO_INTERIORES;
     stat->tiempo_total_produccion += tiempo;
+    stat->tiempo_por_pieza[ESTACION_INTERIORES] += tiempo;
+    stat->costo_pieza[ESTACION_INTERIORES] += PRECIO_INTERIORES;
     sem_post(sem); // V()
 }
 
@@ -187,28 +228,34 @@ void pintura(t_estadisticas *stat, sem_t *sem)
 {
     int prob = (rand() % 100) + 1;
     
-    int costo = (rand() % (PRECIO_PINTURA / 2)) + PRECIO_PINTURA;
     int tiempo = (rand() % (TIEMPO_PINTURA / 2)) + TIEMPO_PINTURA;
 
+    if(prob<=90)
+        printf("Pintura instalado\n");
     while (prob > 90)
     {
         printf("pintura defectuosa, cambiando pintura...\n");
         sem_wait(sem); // P()
         stat->partes_defectuosas++;
         stat->tiempo_total_produccion += tiempo;
-        stat->costo_total += costo;
+        stat->costo_total += PRECIO_PINTURA;
+        stat->defectos_por_pieza[ESTACION_PINTURA] += 1;
+        stat->tiempo_por_pieza[ESTACION_PINTURA] += tiempo;
+        stat->costo_pieza[ESTACION_PINTURA] += PRECIO_PINTURA;
+        stat->costo_defectuosas[ESTACION_PINTURA] += PRECIO_PINTURA;
         sem_post(sem); // V()
 
         sleep(TIEMPO_ESTACION);
 
         prob = (rand() % 100) + 1;
-        costo = (rand() % (PRECIO_PINTURA / 2)) + PRECIO_PINTURA;
         tiempo = (rand() % (TIEMPO_PINTURA / 2)) + TIEMPO_PINTURA;
     }
 
     sem_wait(sem); // P()
-    stat->costo_total += costo;
+    stat->costo_total += PRECIO_PINTURA;
     stat->tiempo_total_produccion += tiempo;
+    stat->tiempo_por_pieza[ESTACION_PINTURA] += tiempo;
+    stat->costo_pieza[ESTACION_PINTURA] += PRECIO_PINTURA;
     sem_post(sem); // V()
 }
 
@@ -216,6 +263,7 @@ void pintura(t_estadisticas *stat, sem_t *sem)
 
 int main()
 {
+
     srand(time(NULL));
     int i;
 
@@ -235,88 +283,181 @@ int main()
                                                             0);
     
     close(idMemoria);
+   
 
-    // Se crea un semáforo con valor inicial 1 (binario).
-    
+    estadisticas->autos_producidos = 0;
+    estadisticas->partes_defectuosas = 0;
+    estadisticas->tiempo_total_produccion = 0.0;
+    estadisticas->costo_total = 0.0;
+
+for (i = 0; i < ESTACIONES_TRABAJO; i++) {
+    estadisticas->defectos_por_pieza[i] = 0;
+    estadisticas->tiempo_por_pieza[i] = 0.0;
+    estadisticas->costo_pieza[i] = 0.0;
+    estadisticas->costo_defectuosas[i] = 0.0;
+}
+
+
+     // Se crea un semáforo con valor inicial 1 (binario) para la memoria comparida.
+
     sem_t *semaforo = sem_open("semaforo",
                                O_CREAT,
                                0600, // r+w user
                                1);
 
-    pid_t pid;
+    // Semaforos para sincronizar los procesos (que no se instale el motor sin chasis y cosas asi...) 
+    sem_t *sem_motor = sem_open("sem_motor",
+                               O_CREAT,
+                               0600, // r+w user
+                               0);
+            
+    sem_t *sem_vidrios = sem_open("sem_vidrios",
+                               O_CREAT,
+                               0600,
+                               0);
 
-    /*
-    Cada fase del ensamblaje es realizada por un proceso hijo que itera sobre todos los autos.
-    Se usa fork para cada uno, y waitpid para esperar su finalización antes de pasar a la siguiente etapa.
-    */
+    sem_t *sem_interior = sem_open("sem_interior",
+                               O_CREAT,
+                               0600,
+                               0);
 
+    sem_t *sem_pintura = sem_open("sem_pintura",
+                               O_CREAT,
+                               0600,
+                               0);
+
+
+    // un pid para cada estacion de trabajo, asi tengo un porceso hijo por cada una
+    pid_t pids[ESTACIONES_TRABAJO];
+
+
+    // Cada fase del ensamblaje es realizada por un proceso hijo que itera sobre todos los autos.
+    // Se usa fork para cada uno.
+    
     // Fase: CHASIS
-    if ((pid = fork()) == 0)
+    if ((pids[ESTACION_CHASIS] = fork()) == 0)
     {
         for (i = 0; i < MAX_AUTOS; i++)
         {
+            printf("\n\nAuto: %d\n", i+1);
             chasis(estadisticas, semaforo);
+            sem_post(sem_motor);
         }
         exit(0);
     }
-    waitpid(pid, NULL, 0);
 
     // Fase: MOTOR
-    if ((pid = fork()) == 0)
+    if ((pids[ESTACION_MOTOR] = fork()) == 0)
     {
         for (i = 0; i < MAX_AUTOS; i++)
         {
+            sem_wait(sem_motor);
+            printf("\n\nAuto: %d\n", i+1);
             motor(estadisticas, semaforo);
+            sem_post(sem_vidrios);
         }
         exit(0);
     }
-    waitpid(pid, NULL, 0);
 
     // Fase: VIDRIOS
-    if ((pid = fork()) == 0)
+    if ((pids[ESTACION_VIDRIOS] = fork()) == 0)
     {
         for (i = 0; i < MAX_AUTOS; i++)
         {
+            sem_wait(sem_vidrios);
+            printf("\n\nAuto: %d\n", i+1);
             vidrios(estadisticas, semaforo);
+            sem_post(sem_interior);
         }
         exit(0);
     }
-    waitpid(pid, NULL, 0);
 
     // Fase: INTERIOR
-    if ((pid = fork()) == 0)
+    if ((pids[ESTACION_INTERIORES] = fork()) == 0)
     {
         for (i = 0; i < MAX_AUTOS; i++)
         {
+            sem_wait(sem_interior);
+            printf("\n\nAuto: %d\n", i+1);
             interior(estadisticas, semaforo);
+            sem_post(sem_pintura);
         }
         exit(0);
     }
-    waitpid(pid, NULL, 0);
 
     // Fase: PINTURA
-    if ((pid = fork()) == 0)
+    if ((pids[ESTACION_PINTURA] = fork()) == 0)
     {
         for (i = 0; i < MAX_AUTOS; i++)
         {
+            sem_wait(sem_pintura);
+            printf("\n\nAuto: %d\n", i+1);
             pintura(estadisticas, semaforo);
         }
         exit(0);
     }
-    waitpid(pid, NULL, 0);
+    
+
+    // Espero a todos los hijos para generar el informe
+    for (int i = 0; i < ESTACIONES_TRABAJO; i++) {
+    waitpid(pids[i], NULL, 0);
+}
 
     // Imprimir resumen
     printf("\nProducción terminada\n");
+
     printf("Autos producidos: %d\n", MAX_AUTOS);
-    printf("Partes defectuosas: %d\n", estadisticas->partes_defectuosas);
-    printf("Costo total: %.2f $\n", estadisticas->costo_total);
-    printf("Tiempo total: %.2f\n\n", estadisticas->tiempo_total_produccion);
+    printf("\nPartes defectuosas: %d\n", estadisticas->partes_defectuosas);
+
+    printf("Defectos por pieza: \n");
+    printf("Chasis defectuosos: %d\n",estadisticas->defectos_por_pieza[ESTACION_CHASIS]);
+    printf("Motores defectuosos: %d\n",estadisticas->defectos_por_pieza[ESTACION_MOTOR]);
+    printf("Vidrios defectuosos: %d\n",estadisticas->defectos_por_pieza[ESTACION_VIDRIOS]);
+    printf("Interiores defectuosos: %d\n",estadisticas->defectos_por_pieza[ESTACION_INTERIORES]);
+    printf("Pinturas defectuosas: %d\n",estadisticas->defectos_por_pieza[ESTACION_PINTURA]);
+    
+    printf("\nTiempo total: %.2f horas\n", estadisticas->tiempo_total_produccion);
+    printf("Tiempo por pieza en horas \n");
+    printf("Tiempo de chasis: %.2f h\n",estadisticas->tiempo_por_pieza[ESTACION_CHASIS]);
+    printf("Tiempo de motores: %.2f h\n",estadisticas->tiempo_por_pieza[ESTACION_MOTOR]);
+    printf("Tiempo de vidrios: %.2f h\n",estadisticas->tiempo_por_pieza[ESTACION_VIDRIOS]);
+    printf("Tiempo de interiores: %.2f h\n",estadisticas->tiempo_por_pieza[ESTACION_INTERIORES]);
+    printf("Tiempo de pintura: %.2f h\n",estadisticas->tiempo_por_pieza[ESTACION_PINTURA]);
+
+    printf("\nCosto total: %.2f $\n", estadisticas->costo_total);
+    printf("Costo por pieza:\n");
+    printf("Costo de chasis: %.2f $\n",estadisticas->costo_pieza[ESTACION_CHASIS]);
+    printf("Costo de motores: %.2f $\n",estadisticas->costo_pieza[ESTACION_MOTOR]);
+    printf("Costo de vidrios: %.2f $\n",estadisticas->costo_pieza[ESTACION_VIDRIOS]);
+    printf("Costo de interiores: %.2f $\n",estadisticas->costo_pieza[ESTACION_INTERIORES]);
+    printf("Costo de pintura: %.2f $\n",estadisticas->costo_pieza[ESTACION_PINTURA]);
+    printf("Costo por pieza defectuosa:\n");
+    printf("Costo de chasis defectuosos: %.2f $\n",estadisticas->costo_defectuosas[ESTACION_CHASIS]);
+    printf("Costo de motores defectuosos: %.2f $\n",estadisticas->costo_defectuosas[ESTACION_MOTOR]);
+    printf("Costo de vidrios defectuosos: %.2f $\n",estadisticas->costo_defectuosas[ESTACION_VIDRIOS]);
+    printf("Costo de interiores defectuosos: %.2f $\n",estadisticas->costo_defectuosas[ESTACION_INTERIORES]);
+    printf("Costo de pintura defectuosas: %.2f $\n",estadisticas->costo_defectuosas[ESTACION_PINTURA]);
 
 
     // Liberar recursos, se desconecta y elimina la memoria compartida y el semáforo.
 
-    sem_close(semaforo);
     munmap(estadisticas, sizeof(t_estadisticas));
+
+    sem_close(semaforo);
+    sem_close(sem_motor);
+    sem_close(sem_vidrios);
+    sem_close(sem_interior);
+    sem_close(sem_pintura);
+
+
+    sem_unlink("semaforo");
+    sem_unlink("sem_motor");
+    sem_unlink("sem_vidrios");
+    sem_unlink("sem_interior");
+    sem_unlink("sem_pintura");
+
+    munmap(estadisticas, sizeof(t_estadisticas));
+    shm_unlink(NombreMemoria);
 
     return EXIT_SUCCESS;
 }
